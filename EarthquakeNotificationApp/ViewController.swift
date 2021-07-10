857d64a09c75b3bf0973cea95932f852963bc170
Emin Hayal <eminhayal768@gmail.com>
2021-07-10 11:55:30 +0300

    First Build

---
+++ EarthquakeNotificationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EarthquakeNotificationApp
+//
+//  Created by Emin Hayal on 10.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
